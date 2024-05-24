53f801cd35d9a94e4cb14bb1b20224c1112de9fa
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-05-24 18:22:29 +0630

    fetch all recipes from backend api

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ require("dotenv").config();
 const morgan = require("morgan");
 const recipesRoutes = require("./routes/recipes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
 
 const mongoURL =
   "mongodb+srv://spike:test123@mern-cluster.ix9jcsc.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster";
 
+app.use(cors()); // local development -- WARNING---
 app.use(express.json()); //parse json data
-
 app.use(morgan("dev"));
 mongoose.connect(mongoURL).then(() => {
   console.log("connected to db");
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,29 @@
+import { useEffect, useState } from "react"
+import RecipeCard from "../components/RecipeCard";
+
+
 export default function Home() {
+    let [recipes,setRecipes] = useState([]);
+    useEffect(()=>{
+        let fetchRecipes = async()=>{
+        let response = await fetch('http://localhost:4000/api/recipes');
+        if(response.ok){
+            let data = await response.json();
+            setRecipes(data);
+        }
+        }
+
+        fetchRecipes();
+    },[])
+
   return (
     <div className="space-y-3">
-        <div className="bg-white p-5 rounded-2xl space-y-3">
-            <h3 className="text-xl font-bold text-orange-400">How to make a cake?</h3>
-            <p>Description</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorum quos consectetur vel labore, distinctio ad placeat voluptates sint aliquam obcaecati praesentium provident sapiente laboriosam repudiandae, voluptatibus nemo saepe nulla!</p>
-            <div className="space-x-2">
-                <span>Ingredients -</span>
-                <span className="bg-orange-400 text-white px-2 py-1 text-sm rounded-full">3 eggs</span>
-                <span className="bg-orange-400 text-white px-2 py-1 text-sm rounded-full">Baking soda</span>
-            </div>
-            <p className="text-gray-500">Published at - 5 min ago</p>
-        </div>
-        <div className="bg-white p-5 rounded-2xl space-y-3">
-            <h3 className="text-xl font-bold text-orange-400">How to make a cake?</h3>
-            <p>Description</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorum quos consectetur vel labore, distinctio ad placeat voluptates sint aliquam obcaecati praesentium provident sapiente laboriosam repudiandae, voluptatibus nemo saepe nulla!</p>
-            <div className="space-x-2">
-                <span>Ingredients -</span>
-                <span className="bg-orange-400 text-white px-2 py-1 text-sm rounded-full">3 eggs</span>
-                <span className="bg-orange-400 text-white px-2 py-1 text-sm rounded-full">Baking soda</span>
-            </div>
-            <p className="text-gray-500">Published at - 5 min ago</p>
-        </div>
+        {recipes.length && (recipes.map(recipe=>(
+            <RecipeCard key={recipe.id} recipe={recipe}/>
+        ))
+           
+        )}
+        
     </div>
   )
 }
