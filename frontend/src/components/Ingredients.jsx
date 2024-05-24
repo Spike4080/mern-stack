f38871beb9a1b3900aa8fd61973f853d5a241d8a
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-05-24 21:47:45 +0630

    add ingredients feature

--- frontend/src/components/Ingredients.jsx
+++ frontend/src/components/Ingredients.jsx
@@ -1,6 +1,6 @@
 export default function Ingredients({ingredients}) {
   return (
-    <div>
+    <div className="space-x-1">
         <span>Ingredients -</span>
         {!!ingredients.length && ingredients.map((ingredient,i)=>(
         <span className="bg-orange-400 text-white px-2 py-1 text-sm rounded-full" key={i}>{ingredient}</span>
--- frontend/src/pages/RecipeForm.jsx
+++ frontend/src/pages/RecipeForm.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
 import plus from "../assets/plus.svg"
 import Ingredients from "../components/Ingredients"
 
 export default function RecipeForm() {
+    let [ingresients,setIngredients]=useState([]);
+    let [newIngredients,setnewIngredients]=useState('');
+
+    let addIngredient = () => {
+        setIngredients(prevState=>[newIngredients,...prevState]);
+        setnewIngredients('');
+    }
   return (
     <div className="mx-auto max-w-md border-2 border-white p-4">
         <h1 className="mb-6 text-2xl font-bold text-orange-400 text-center">RecipeForm</h1>
@@ -9,11 +17,11 @@ export default function RecipeForm() {
             <input type="text" placeholder="Recipe Title"  className="w-full p-1"/>
             <textarea rows="5" className="w-full p-1" placeholder="Description"></textarea>
            <div className="flex space-x-2 items-center">
-           <input type="text" placeholder="Recipe Ingredient"  className="w-full p-1"/>
-            <img src={plus} alt="" className="cursor-pointer"/>
+           <input value={newIngredients} onChange={e=>setnewIngredients(e.target.value)} type="text" placeholder="Recipe Ingredient"  className="w-full p-1"/>
+            <img src={plus} alt="" className="cursor-pointer" onClick={addIngredient}/>
            </div>
            <div>
-            <Ingredients ingredients={['3 eggs','water']}/>
+            <Ingredients ingredients={ingresients}/>
            </div>
            <button className="w-full px-3 py-1 rounded-full bg-orange-400 text-white">Create Recipe</button>
         </form>
