cb1341467115de73fc9696d8589401ca08005961
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-05-28 15:05:54 +0630

    register api

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -1,11 +1,30 @@
-import User from "../models/User";
+const User = require("../models/User");
+let bcrypt = require("bcrypt");
 
 const UsersController = {
   login: (req, res) => {
     return res.json({ msg: "user login" });
   },
-  register: (req, res) => {
-    return res.json({ msg: " user register" });
+  register: async (req, res) => {
+    try {
+      let { name, email, password } = req.body;
+      let userExists = await User.findOne({ email });
+      if (userExists) {
+        throw new Error("user already exists");
+      }
+
+      let salt = await bcrypt.genSalt();
+      let hashValue = await bcrypt.hash(password, salt);
+
+      let user = await User.create({
+        name,
+        email,
+        password: hashValue,
+      });
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
   },
 };
 
